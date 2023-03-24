e010447c36d9c7ab309590d835fbfb579ebf4fc2
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-03-24 20:27:20 -0300

    first commit

---
+++ src/main/java/br/univille/projfabsoft2023/Projfabsoft2023Application.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoft2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projfabsoft2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projfabsoft2023Application.class, args);
+	}
+
+}
