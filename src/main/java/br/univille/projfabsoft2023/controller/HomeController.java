bbbb76bba1beeaf106d5ad70f67145e2ecb9136d
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-03-27 22:28:50 -0300

    configuração da persistencia em banco de dados relacional

--- src/main/java/br/univille/projfabsoft2023/controller/HomeController.java
+++ src/main/java/br/univille/projfabsoft2023/controller/HomeController.java
@@ -1,5 +1,7 @@
 package br.univille.projfabsoft2023.controller;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,9 @@ public class HomeController {
 
     @GetMapping
     public ModelAndView index(){
-        return new ModelAndView("home/index");
+        HashMap<String, String> map = new HashMap<>();
+        map.put("apelido","valor");
+        map.put("nome","zezinho");
+        return new ModelAndView("home/index",map);
     }
 }
---
+++ src/main/java/br/univille/projfabsoft2023/entity/Cliente.java
@@ -0,0 +1,42 @@
+package br.univille.projfabsoft2023.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(length = 600)
+    private String nome;
+    @Temporal(value = TemporalType.DATE)
+    private Date dataNascimento;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+}
