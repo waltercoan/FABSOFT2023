a898cfbdf0f46f8d0fdf8b0053f04090b7efc3c7
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-04-28 20:10:40 -0300

    remover registro

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,19 @@
+(function(){
+    $("#tabclientes").on("click",".js-delete", function(){
+        let botaoClicado = $(this);
+        $("#btnsim").attr("data-id", botaoClicado.attr("data-id"));
+        $("#modalcliente").modal("show");
+    });
+    $("#btnsim").on("click", function(){
+        let botaoSim = $(this);
+        let id = botaoSim.attr("data-id");
+        $.ajax({
+            url: "/cliente/remover/" + id,
+            method: "GET",
+            success: function(){
+                window.location.href = "/cliente";
+            }
+        });
+    });
+
+})();
