b02702822a9c9ebc6307c355a9b24fb3b67374cd
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-05-22 17:57:33 -0300

    auth AAD

--- src/main/java/br/univille/projfabsoft2023/controller/ClienteController.java
+++ src/main/java/br/univille/projfabsoft2023/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.univille.projfabsoft2023.controller;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import br.univille.projfabsoft2023.service.CidadeService;
 import br.univille.projfabsoft2023.service.ClienteService;
 
 @Controller
+@PreAuthorize("hasAuthority('APPROLE_admin')")
 @RequestMapping("/cliente")
 public class ClienteController {
     
